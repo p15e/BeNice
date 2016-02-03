94b4e7d6931dbbcdc7f60f9d0d87c1ebf03c7b42
p15e <pedro.cardosoleite@unige.ch>
2016-02-03 17:14:05 +0100

    added methods to print tables for principal and betareg; still lots of work to do on this.

--- printTest.R
+++ printTest.R
@@ -100,3 +100,125 @@ pBiggerThan <- function(pvalues, ndigits = 2){
 
 
 
+
+
+# function to remove multiple spaces
+trim <- function(x) return(gsub("^ *|(?<= ) | *$", "", x, perl=T))
+
+
+principal2df <- function(pc){
+        nFactors <- pc$factors
+        nVar <- length(pc$values)
+        
+        pc.out <- capture.output(pc)
+        # 
+        # pc.out[1]
+        # suggest caption:
+        # tt <- paste0(pc.out[1], '. ', pc.out[2], '. ', pc.out[3], '.')
+        
+        
+        nr.start <- 5
+        nr.end <- nr.start+nVar - 1 #25
+        nc.start <- 1
+        nc.end <- nFactors + 1
+        
+        
+        pc.form <- pc.out[nr.start:nr.end]
+        # pc.form[1]
+        pc.form <- paste0(pc.form, ';')
+        
+        lines <- gsub(" ", ";", trim(pc.form))
+        con <- textConnection(lines)
+        data <- read.csv(con, sep=";", header=F)
+        close(con)
+        
+        data <- as.data.frame(data)
+        
+        df <- data[,1:nc.end]
+        names(df) <- c('', paste0('Comp.', 1:7))
+        df
+}
+
+
+
+
+
+
+principal.print <- function(pcdf, lowerValue=0.2, upperValue=.4){
+        
+        if (is.data.frame(pcdf)){
+                
+        } else {
+                pc.out <- capture.output(pcdf)
+                # suggest caption:
+                cap <- paste0(pc.out[1], '. ', pc.out[2], '. ', pc.out[3], '.')
+                
+                pcdf <- principal2df(pcdf)
+        }
+        
+        
+        # pc.table <- xtable(pcdf)
+        
+        
+        
+        
+        # threshold output by cor value:
+        varNames <- pcdf[,1]
+        coefValues <- pcdf[,-1]
+        
+        if (lowerValue < 0 | lowerValue > 1){
+                browser()       
+        }
+        if (upperValue < 0 | upperValue > 1 | upperValue <= lowerValue){
+                browser()       
+        }
+        
+        # remove lower values
+        if (lowerValue > 0){
+                coefValueCHR <- coefValues
+                coefValueCHR[abs(coefValues) < lowerValue] <- ''
+        }
+        
+        
+        
+        # bold high values
+        if (upperValue > 0){
+                coefValueCHR[abs(coefValues) >= upperValue] <- paste0('**', coefValueCHR[abs(coefValues) >= upperValue], '**')
+                
+        }
+        
+        
+        # df <- cbind(varNames, coefValueCHR)
+        # pc.table <- xtable(df)
+        xtable(cbind(varNames, coefValueCHR))
+}
+
+
+
+
+p2stars <- function(pv){
+        s <- rep('', length(pv))
+        s[pv <= .1] <- '.'
+        s[pv <= .05] <- '*'
+        s[pv <= .01] <- '**'
+        s[pv <= .001] <- '***'
+        s
+}
+
+beta.print <- function(beta.fit){
+        require(xtable)
+        aa <- as.data.frame(summary(beta.fit)$coefficients$mean)[-1,]
+        aa$stars <- p2stars(aa$`Pr(>|z|)`)
+        names(aa) <- c("coef.", "se.", "z", "p", " ")
+        xtable(aa)       
+}
+
+
+
+### COMPUTATIONS
+# if there are any 0 or 1 this code will bug.
+# transfor variables:
+prior.beta <- function(yy, br=0.001){
+        yy * (1-2*br) + br
+}
+
